a0dc1259c527ca4f2bb3e9c67be02286519e1acb
mahmudgithub <mahmudhossain838@gmail.com>
2021-04-06 08:11:06 +0100

    react first check

--- fontend/font_side/src/App.js
+++ fontend/font_side/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         <Test/>
         </a>
       </header>
     </div>
---
+++ fontend/font_side/src/components/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+    return (
+        <div>
+            hello i am mahmud hossain
+        </div>
+    )
+}
+
+export default Test
